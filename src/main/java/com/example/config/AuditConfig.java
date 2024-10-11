abddb4293577e428d1d598f3d1cc3ad5081b7990
ishig <ishig@DESKTOP-DC3FJH7>
2024-10-11 14:17:26 +0900

    外部キー

--- src/main/java/com/example/config/AuditConfig.java
+++ src/main/java/com/example/config/AuditConfig.java
@@ -23,7 +23,10 @@ public class AuditConfig {
         		if (auth == null || !auth.isAuthenticated() || auth.getPrincipal() == null) {
         			return Optional.empty();
         		}
-        		LoginUserDetails principal = (LoginUserDetails) auth.getPrincipal();
+        		
+    			//return Optional.of(1);//初期ユーザ作成時のみ有効化
+
+    			LoginUserDetails principal = (LoginUserDetails) auth.getPrincipal();
         		return Optional.of(principal.getId());
         	}
         };
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 --ログインテスト用ユーザ（パスワードはtest）
+--insertしたユーザでログインエラーになる（暗号文が異なる）場合は
+--プログラムから作成する（AuditConfigとSecurityConfigとnav.htmlの調整要）
 insert into local_user
 (name, email, password, enabled, ver, created_by, created_date, modified_by, modified_date)
 select 'テスト用', 'test@example.com'
